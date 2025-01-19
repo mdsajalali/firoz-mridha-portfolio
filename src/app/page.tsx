f80c8473bc507c7ba0f8cc85f5dbf309828fb522
mdsajalali <mdsajalali2020@gmail.com>
2025-01-19 22:25:02 +0600

    add: intro section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Footer from "../components/Footer";
+import Intro from "@/components/Intro";
 
 const page = () => {
   return (
     <>
       <Header />
       <Hero />
+      <Intro />
       <Footer />
     </>
   );
